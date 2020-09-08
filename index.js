39570525de297e6de6d7923e6a686258f9cef55b
Christopher Imrie <ckimrie@gmail.com>
2020-09-08 13:48:41 +0100

    Adds hashing to long env id names

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ async function init() {
     const gitShortSha = process.env.GITHUB_SHA.substr(0,8);
     const gitRef = (process.env.GITHUB_REF || "").replace('refs/heads/', '').replace('refs/tags/', '');
     console.log(`Git ref [${gitRef}]`);
-    
+
     const name = core.getInput('name');
     console.log(`Received name [${name}]`);
     const prefix = core.getInput('prefix');
     console.log(`Received prefix [${prefix}]`);
     const suffix = core.getInput('suffix');
     console.log(`Received prefix [${suffix}]`);
-    
-    let envId = slugify(name ? name : gitRef);
-    
+
+    let envName = name ? name : gitRef;
+    let envId = slugify(envName);
+
     if(envId.length > 20){
-        envId = envId.substr(0, 20);
+        envId = envId.substr(0, 15) + hash(envName);
     }
-    
+
     if(prefix) {
         envId = prefix + '-' + envId;
     }
@@ -25,7 +26,7 @@ async function init() {
     if(suffix) {
         envId = envId + '-' + suffix;
     }
-    
+
     core.setOutput("id", envId);
 }
 
@@ -44,6 +45,10 @@ function slugify(string) {
     .replace(/-+$/, '') // Trim - from end of text
 }
 
+function hash(str) {
+    return str.split('').reduce((prevHash, currVal) =>
+        (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+}
 
 init().catch(err => {
     core.setFailed(`Action failed with error ${err}`);
